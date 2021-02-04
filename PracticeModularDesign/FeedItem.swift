747a054b23e32f86ad3d3b55f5b7af9d7fa9920d
Marko Tribl <triblmarko@gmail.com>
2021-02-04 23:08:24 +0100

    Create FeedItem model

---
+++ PracticeModularDesign/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  PracticeModularDesign
+//
+//  Created by Marko Tribl on 2/4/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUrl: URL
+}
