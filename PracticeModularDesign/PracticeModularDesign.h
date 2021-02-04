56f438b14fdca24a32d3648f5cfba5bcc7651552
Marko Tribl <triblmarko@gmail.com>
2021-02-04 22:55:05 +0100

    Initial commit

---
+++ PracticeModularDesign/PracticeModularDesign.h
@@ -0,0 +1,18 @@
+//
+//  PracticeModularDesign.h
+//  PracticeModularDesign
+//
+//  Created by Marko Tribl on 2/4/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PracticeModularDesign.
+FOUNDATION_EXPORT double PracticeModularDesignVersionNumber;
+
+//! Project version string for PracticeModularDesign.
+FOUNDATION_EXPORT const unsigned char PracticeModularDesignVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PracticeModularDesign/PublicHeader.h>
+
+
