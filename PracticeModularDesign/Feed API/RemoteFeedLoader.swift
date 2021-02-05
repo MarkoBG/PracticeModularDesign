75e2c70a5a57ba4da6b98bf40fdb564601c60af4
Marko Tribl <triblmarko@gmail.com>
2021-02-05 22:51:49 +0100

    Move 'RemoteFeedLoader; components to production

---
+++ PracticeModularDesign/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,29 @@
+//
+//  RemoteFeedLoader.swift
+//  PracticeModularDesign
+//
+//  Created by Marko Tribl on 2/5/21.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
+
+
