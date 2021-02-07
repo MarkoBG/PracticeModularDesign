7e8caa028886cab7fe55a2a9fce8459e53603680
Marko Tribl <triblmarko@gmail.com>
2021-02-07 21:09:20 +0100

    Delivers invalidData error on non 200 HTTP responses

--- PracticeModularDesignTests/RemoteFeedLoaderTests.swift
+++ PracticeModularDesignTests/RemoteFeedLoaderTests.swift
@@ -48,22 +48,40 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
     
+    func test_load_deliversErrorOnNon200HTTPResponse() {
+        let anyURL = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: anyURL)
+        
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        
+        client.complete(withSatutsCode: 400)
+        
+        XCTAssertEqual(capturedErrors, [.invalidData])
+    }
+    
     // MARK: - Helper
     
     private class HTTPClientSpy: HTTPClient {
 
-        private var messages = [(url: URL, completion: (Error) -> Void)]()
+        private var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
         
         var requestedURLs: [URL] {
             return messages.map { $0.url }
         }
         
-        func get(from url: URL, completion: @escaping (Error) -> Void) {
+        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
             messages.append((url, completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error)
+            messages[index].completion(error, nil)
+        }
+        
+        func complete(withSatutsCode code: Int, at index: Int = 0) {
+            let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)
+            
+            messages[index].completion(nil, response)
         }
     }
     
