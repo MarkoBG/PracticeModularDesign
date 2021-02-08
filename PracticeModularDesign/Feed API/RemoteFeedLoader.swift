c001b684342d163d8e4bafe04296067f6862f00c
Marko Tribl <triblmarko@gmail.com>
2021-02-08 21:29:12 +0100

    Move FeedItemsMapper to its own file

--- PracticeModularDesign/Feed API/RemoteFeedLoader.swift
+++ PracticeModularDesign/Feed API/RemoteFeedLoader.swift
@@ -7,8 +7,6 @@
 
 import Foundation
 
-
-
 public final class RemoteFeedLoader {
     
     private let url: URL
@@ -45,35 +43,6 @@ public final class RemoteFeedLoader {
                 completion(.failure(.connectivity))
             }
         }
-    }
-}
-
-private class FeedItemsMapper {
-    
-    private struct Root: Decodable {
-        let items: [Item]
-    }
-
-    private struct Item: Decodable {
-        public let id: UUID
-        public let description: String?
-        public let location: String?
-        public let image: URL
-        
-        var item: FeedItem {
-            return FeedItem(id: id, description: description, location: location, imageURL: image)
-        }
-    }
-    
-    static var OK_200 = 200
-    
-    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == OK_200 else {
-            throw RemoteFeedLoader.Error.invalidData
-        }
-        
-        return try JSONDecoder().decode(Root.self, from: data).items.map { $0.item }
-        
     }
 }
 
